2dfb013d9908ad8891ada6068a9fab72af5e1f3b
Mirumo <mysteryperson520@gmail.com>
2025-08-06 20:03:00 +0800

    chore: remove useless achievement

--- src/data/AchievementsData.ts
+++ src/data/AchievementsData.ts
@@ -10,14 +10,6 @@ export const achievements: Medal[] = [
   { id: 2, icon: 'mdi:account-group', name: '開始融入', unlocked: false }, // 聊天室發 8 則留言
   { id: 3, icon: 'mdi:forum', name: '意見領袖', unlocked: false }, // 聊天室發 20 則留言
 
-  { id: 4, icon: 'mdi:account-heart', name: '有人理我', unlocked: false }, // 聊天室留言獲得 1 個愛心
-  { id: 5, icon: 'mdi:sparkles', name: '人氣新星', unlocked: false }, // 聊天室留言獲得 30 個愛心
-  { id: 6, icon: 'mdi:fire', name: '魅力無法擋', unlocked: false }, // 聊天室留言獲得 100 個愛心
-
-  { id: 7, icon: 'mdi:hand-heart', name: '給你一顆愛心', unlocked: false }, // 按別人留言愛心 1 次
-  { id: 8, icon: 'mingcute:user-heart-fill', name: '暖心使者', unlocked: false }, // 按別人留言愛心 10 次
-  { id: 9, icon: 'mdi:cards-heart', name: '真心不騙', unlocked: false }, // 按別人留言愛心 30 次
-
   { id: 10, icon: 'mingcute:sunrise-fill', name: '早起的鳥兒有蟲吃', unlocked: false }, // 參與第一天或第二天的開幕
   { id: 11, icon: 'mdi:flag-checkered', name: '最後一哩路', unlocked: false }, // 參與第一天的閉幕
   { id: 12, icon: 'mdi:infinity', name: '有始有終', unlocked: false }, // 參與開幕與閉幕
